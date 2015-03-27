b9152cd5a1fc61327e9609beedd4462ff70dc86c
Administrator <admin@local.host>
2015-03-27 21:53:44 +0100

    Updated Several Things ...

---
+++ config/application.js
@@ -0,0 +1,34 @@
+/**
+ * Created by NicholasE on 27/03/2015.
+ */
+module.exports = {
+
+  // Name of the application (used as default <title>)
+  appName: "Invoicing",
+
+  // Port this Sails application will live on
+  port: 80,
+  subdomain: 'invoicing.gitlab.be',
+
+  // The environment the app is deployed in
+  // (`development` or `production`)
+  //
+  // In `production` mode, all css and js are bundled up and minified
+  // And your views and templates are cached in-memory.  Gzip is also used.
+  // The downside?  Harder to debug, and the server takes longer to start.
+  environment: 'development',
+
+  // Logger
+  // Valid `level` configs:
+  //
+  // - error
+  // - warn
+  // - debug
+  // - info
+  // - verbose
+  //
+  log: {
+    level: 'info'
+  }
+
+};
